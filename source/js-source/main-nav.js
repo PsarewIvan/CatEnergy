22cbbdf44ee0ea926f61e138b9e696cd2eab22d5
Ivan Psarev <psarew.ivan@yandex.ru>
2019-05-02 23:05:36 +0300

    Стилизация шапки

---
+++ source/js-source/main-nav.js
@@ -0,0 +1,5 @@
+var mainNav = document.querySelector('.js--main-nav');
+var mainNavButton = document.querySelector('.js--header__nav-btn');
+
+mainNav.classList.add('display-none');
+mainNavButton.classList.remove('header__nav-btn--close');
