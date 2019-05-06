eeabea9268fb353496d7bf9417c53bfc68a28c64
Ivan Psarev <psarew.ivan@yandex.ru>
2019-05-06 22:04:36 +0300

    Поправил отступы, добавил скрипт появеления меню при изменении ширины экрана

--- source/js-source/main-nav.js
+++ source/js-source/main-nav.js
@@ -1,5 +1,19 @@
 var mainNav = document.querySelector('.js--main-nav');
 var mainNavButton = document.querySelector('.js--header__nav-btn');
 
-mainNav.classList.add('display-none');
-mainNavButton.classList.remove('header__nav-btn--close');
+if (matchMedia) {
+  var mq = window.matchMedia( "(min-width: 748px)" );
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
+
+function WidthChange(mq) {
+  if (!mq.matches) {
+    mainNav.classList.add('display-none');
+    mainNavButton.classList.remove('display-none');
+    mainNavButton.classList.remove('header__nav-btn--close');
+  } else {
+    mainNav.classList.remove('display-none');
+    mainNavButton.classList.add('display-none');
+  }
+}
