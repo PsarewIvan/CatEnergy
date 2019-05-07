1a725974a64e833fd235fd0cca828316e2d5cb50
Ivan Psarev <psarew.ivan@yandex.ru>
2019-05-07 15:21:57 +0300

    Стилизовал шапку для планшета

--- source/js-source/main-nav.js
+++ source/js-source/main-nav.js
@@ -2,10 +2,21 @@ var mainNav = document.querySelector('.js--main-nav');
 var mainNavButton = document.querySelector('.js--header__nav-btn');
 
 if (matchMedia) {
-  var mq = window.matchMedia( "(min-width: 748px)" );
+  var mq = window.matchMedia( "(min-width: 768px)" );
   mq.addListener(WidthChange);
   WidthChange(mq);
 }
+
+mainNavButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  if (mainNav.classList.contains('display-none')) {
+    mainNav.classList.remove('display-none');
+    mainNavButton.classList.add('header__nav-btn--close')
+  } else {
+    mainNav.classList.add('display-none');
+    mainNavButton.classList.remove('header__nav-btn--close')
+  }
+});
 
 function WidthChange(mq) {
   if (!mq.matches) {
