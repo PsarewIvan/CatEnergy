5165b3c92c0df6cd4625cd7d40e66ba9a223b572
Ivan Psarev <psarew.ivan@yandex.ru>
2019-05-25 18:04:15 +0300

    Updates

--- js/script-min.js
+++ js/script-min.js
@@ -1,1 +1,124 @@
-"use strict";var t=document.querySelector(".js-example__switch-btn--after"),a=document.querySelector(".js-example__switch-btn--before"),s=document.querySelector(".js-example__switch"),n=document.querySelector(".js-example__picture--after"),i=document.querySelector(".js-example__picture--before"),c="example__picture--hide";if(matchMedia){var e=window.matchMedia("(max-width: 768px)");e.addListener(r),r(e)}function r(e){e.matches?(t.addEventListener("click",o),a.addEventListener("click",l),s.addEventListener("click",d)):(t.removeEventListener("click",o),a.removeEventListener("click",l),s.removeEventListener("click",d))}function o(e){e.preventDefault(),n.classList.remove(c),i.classList.add(c),s.classList.add("example__switch--after")}function l(e){e.preventDefault(),i.classList.remove(c),n.classList.add(c),s.classList.remove("example__switch--after")}function d(e){e.preventDefault(),i.classList.toggle(c),n.classList.toggle(c),s.classList.toggle("example__switch--after")}ymaps.ready(function(){if(document.querySelector("#ymap")){var e=new ymaps.Map("ymap",{center:[59.938631,30.3230554],zoom:17}),t=new ymaps.Placemark([59.938631,30.3230554],{balloonContentHeader:"Cat Energy",balloonContentBody:"ул. Большая <br>Конюшенная, д. 19/8 <br>Санкт-Петербург",hintContent:"Мы находимся здесь"},{iconLayout:"default#image",iconImageHref:"../img/picture/map-pin.png",iconImageSize:[45,38],iconImageOffset:[-22,-38]});e.geoObjects.add(t),e.behaviors.disable("drag")}});var m=document.querySelector(".js--main-nav"),p=document.querySelector(".js--header__nav-btn");if(matchMedia){var u=window.matchMedia("(min-width: 768px)");u.addListener(v),v(u)}function v(e){e.matches?(m.classList.remove("display-none"),p.classList.add("display-none")):(m.classList.add("display-none"),p.classList.remove("display-none"),p.classList.remove("header__nav-btn--close"))}p.addEventListener("click",function(e){e.preventDefault(),m.classList.contains("display-none")?(m.classList.remove("display-none"),p.classList.add("header__nav-btn--close")):(m.classList.add("display-none"),p.classList.remove("header__nav-btn--close"))});
+"use strict";
+
+var exampleAfterBtn = document.querySelector('.js-example__switch-btn--after');
+var exampleBeforeBtn = document.querySelector('.js-example__switch-btn--before');
+var exampleToggle = document.querySelector('.js-example__switch');
+var imgAfter = document.querySelector('.js-example__picture--after');
+var imgBefore = document.querySelector('.js-example__picture--before');
+var imgHide = 'example__picture--hide';
+
+if (matchMedia) {
+  var mqExample = window.matchMedia('(max-width: 768px)');
+  mqExample.addListener(WidthChangeExample);
+  WidthChangeExample(mqExample);
+}
+
+function WidthChangeExample(mq) {
+  if (mq.matches) {
+    exampleAfterBtn.addEventListener('click', showImgAfter);
+    exampleBeforeBtn.addEventListener('click', showImgBefore);
+    exampleToggle.addEventListener('click', toggleImg);
+  } else {
+    exampleAfterBtn.removeEventListener('click', showImgAfter);
+    exampleBeforeBtn.removeEventListener('click', showImgBefore);
+    exampleToggle.removeEventListener('click', toggleImg);
+  }
+}
+
+function showImgAfter(evt) {
+  evt.preventDefault();
+  imgAfter.classList.remove(imgHide);
+  imgBefore.classList.add(imgHide);
+  exampleToggle.classList.add('example__switch--after');
+}
+
+function showImgBefore(evt) {
+  evt.preventDefault();
+  imgBefore.classList.remove(imgHide);
+  imgAfter.classList.add(imgHide);
+  exampleToggle.classList.remove('example__switch--after');
+}
+
+function toggleImg(evt) {
+  evt.preventDefault();
+  imgBefore.classList.toggle(imgHide);
+  imgAfter.classList.toggle(imgHide);
+  exampleToggle.classList.toggle('example__switch--after');
+}
+"use strict";
+
+if (matchMedia && document.querySelector('#ymap')) {
+  ymaps.ready(function () {
+    var mapWidth = window.matchMedia('(max-width: 768px)');
+    var myMap = new ymaps.Map("ymap", {
+      center: [59.938631, 30.3230554],
+      zoom: 17
+    });
+    var placemarkMobile = new ymaps.Placemark([59.938631, 30.3230554], {
+      balloonContentHeader: "Cat Energy",
+      balloonContentBody: "ул. Большая <br>Конюшенная, д. 19/8 <br>Санкт-Петербург",
+      hintContent: "Мы находимся здесь"
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/picture/map-pin.png',
+      iconImageSize: [45, 38],
+      iconImageOffset: [-22, -38]
+    });
+    var placemarkTablet = new ymaps.Placemark([59.938631, 30.3230554], {
+      balloonContentHeader: "Cat Energy",
+      balloonContentBody: "ул. Большая <br>Конюшенная, д. 19/8 <br>Санкт-Петербург",
+      hintContent: "Мы находимся здесь"
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/picture/map-pin.png',
+      iconImageSize: [90, 76],
+      iconImageOffset: [-22, -38]
+    });
+    mapWidth.addListener(addPlacemark);
+    addPlacemark(mapWidth);
+    myMap.behaviors.disable(['drag', 'scrollZoom']);
+  });
+}
+
+function addPlacemark(mq) {
+  if (mq.matches) {
+    myMap.geoObjects.remove(placemarkTablet);
+    myMap.geoObjects.add(placemarkMobile);
+  } else {
+    myMap.geoObjects.remove(placemarkMobile);
+    myMap.geoObjects.add(placemarkTablet);
+  }
+}
+"use strict";
+
+var mainNav = document.querySelector('.js--main-nav');
+var mainNavButton = document.querySelector('.js--header__nav-btn');
+
+if (matchMedia) {
+  var mqNav = window.matchMedia('(min-width: 768px)');
+  mqNav.addListener(WidthChangeNav);
+  WidthChangeNav(mqNav);
+}
+
+mainNavButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  if (mainNav.classList.contains('display-none')) {
+    mainNav.classList.remove('display-none');
+    mainNavButton.classList.add('header__nav-btn--close');
+  } else {
+    mainNav.classList.add('display-none');
+    mainNavButton.classList.remove('header__nav-btn--close');
+  }
+});
+
+function WidthChangeNav(mq) {
+  if (!mq.matches) {
+    mainNav.classList.add('display-none');
+    mainNavButton.classList.remove('display-none');
+    mainNavButton.classList.remove('header__nav-btn--close');
+  } else {
+    mainNav.classList.remove('display-none');
+    mainNavButton.classList.add('display-none');
+  }
+}
