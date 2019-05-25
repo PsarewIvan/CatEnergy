e6daf8a3f12c8821f8cfbcec13d3d746a687b292
Ivan Psarev <psarew.ivan@yandex.ru>
2019-05-26 00:30:38 +0300

    Добавил проверку наличия блока сравнения для его скрипта

--- source/js-source/example__mobile.js
+++ source/js-source/example__mobile.js
@@ -1,44 +1,47 @@
-const exampleAfterBtn = document.querySelector('.js-example__switch-btn--after');
-const exampleBeforeBtn = document.querySelector('.js-example__switch-btn--before');
-const exampleToggle = document.querySelector('.js-example__switch');
-const imgAfter = document.querySelector('.js-example__picture--after');
-const imgBefore = document.querySelector('.js-example__picture--before');const imgHide = 'example__picture--hide';
+if (document.querySelector('.js-example__switch')) {
+  const exampleAfterBtn = document.querySelector('.js-example__switch-btn--after');
+  const exampleBeforeBtn = document.querySelector('.js-example__switch-btn--before');
+  const exampleToggle = document.querySelector('.js-example__switch');
+  const imgAfter = document.querySelector('.js-example__picture--after');
+  const imgBefore = document.querySelector('.js-example__picture--before');
+  const imgHide = 'example__picture--hide';
 
-if (matchMedia) {
-  let mqExample = window.matchMedia( '(max-width: 768px)' );
-  mqExample.addListener(WidthChangeExample);
-  WidthChangeExample(mqExample);
-}
-
-function WidthChangeExample(mq) {
-  if (mq.matches) {
-    exampleAfterBtn.addEventListener('click', showImgAfter);
-    exampleBeforeBtn.addEventListener('click', showImgBefore);
-    exampleToggle.addEventListener('click', toggleImg);
-  } else {
-    exampleAfterBtn.removeEventListener('click', showImgAfter);
-    exampleBeforeBtn.removeEventListener('click', showImgBefore);
-    exampleToggle.removeEventListener('click', toggleImg);
+  if (matchMedia) {
+    let mqExample = window.matchMedia( '(max-width: 768px)' );
+    mqExample.addListener(WidthChangeExample);
+    WidthChangeExample(mqExample);
   }
-}
 
-function showImgAfter(evt) {
-  evt.preventDefault();
-  imgAfter.classList.remove(imgHide);
-  imgBefore.classList.add(imgHide);
-  exampleToggle.classList.add('example__switch--after');
-}
-
-function showImgBefore(evt) {
-  evt.preventDefault();
-  imgBefore.classList.remove(imgHide);
-  imgAfter.classList.add(imgHide);
-  exampleToggle.classList.remove('example__switch--after');
-}
-
-function toggleImg(evt) {
-  evt.preventDefault();
-  imgBefore.classList.toggle(imgHide);
-  imgAfter.classList.toggle(imgHide);
-  exampleToggle.classList.toggle('example__switch--after');
+  function WidthChangeExample(mq) {
+    if (mq.matches) {
+      exampleAfterBtn.addEventListener('click', showImgAfter);
+      exampleBeforeBtn.addEventListener('click', showImgBefore);
+      exampleToggle.addEventListener('click', toggleImg);
+    } else {
+      exampleAfterBtn.removeEventListener('click', showImgAfter);
+      exampleBeforeBtn.removeEventListener('click', showImgBefore);
+      exampleToggle.removeEventListener('click', toggleImg);
+    }
+  }
+  
+  function showImgAfter(evt) {
+    evt.preventDefault();
+    imgAfter.classList.remove(imgHide);
+    imgBefore.classList.add(imgHide);
+    exampleToggle.classList.add('example__switch--after');
+  }
+  
+  function showImgBefore(evt) {
+    evt.preventDefault();
+    imgBefore.classList.remove(imgHide);
+    imgAfter.classList.add(imgHide);
+    exampleToggle.classList.remove('example__switch--after');
+  }
+  
+  function toggleImg(evt) {
+    evt.preventDefault();
+    imgBefore.classList.toggle(imgHide);
+    imgAfter.classList.toggle(imgHide);
+    exampleToggle.classList.toggle('example__switch--after');
+  }
 }
