5e6c0d785bc5e6a4163b138027d7e4dd7ede7cf2
Ivan Psarev <psarew.ivan@yandex.ru>
2019-06-11 12:09:16 +0300

    Модернизировал сборщик, переписал пути JS, добавил минификацию js, html

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
+const rigger = require('gulp-rigger');
 
 const posthtml = require('gulp-posthtml');
 const include = require('posthtml-include');
+const htmlmin = require('gulp-htmlmin');
 
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
@@ -24,17 +26,54 @@ const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
 const ghpages = require('gh-pages');
 
+const path = {
+  build: {
+    root: 'build/',
+    html: 'build/',
+    js: 'build/js/',
+    style: 'build/css/',
+    img: 'build/img/picture',
+    imgWebp: 'build/img/webp/',
+    fonts: 'build/fonts/',
+    svg: 'build/img/icon/',
+    svgSprite: 'build/svg/'
+  },
+  source: {
+    html: 'source/*.html',
+    js: 'source/js/main.js',
+    style: 'source/less/main.less',
+    img: 'source/img/picture/*.{jpg,png}',
+    fonts: 'source/fonts/**/*.{woff,woff2}',
+    svg: 'source/img/icon/**/*.svg',
+    svgSprite: 'source/img/sprite/*.svg'
+  },
+  watch: {
+    html: 'source/*.html',
+    js: 'source/js/**/*.js',
+    style: 'source/less/**/*.less',
+    img: 'source/img/**/*.*',
+    fonts: 'source/fonts/**/*.{woff,woff2}'
+  },
+  clean: {
+    all: 'build/*',
+    noImg: '!build/img',
+    img: 'build/img/*'
+  }
+};
+
 function html() {
-  return gulp.src('source/*.html')
+  return gulp.src(path.source.html)
+              .pipe(rigger())
               .pipe(posthtml([
                 include()
               ]))
-              .pipe(gulp.dest('build'))
+              .pipe(htmlmin({ collapseWhitespace: true }))
+              .pipe(gulp.dest(path.build.html))
               .pipe(browserSync.stream());
 }
 
 function styles() {
-  return gulp.src('source/less/main.less')
+  return gulp.src(path.source.style)
               .pipe(plumber())
               .pipe(sourcemaps.init())
               .pipe(less())
@@ -45,55 +84,58 @@ function styles() {
               .pipe(cleanCSS({level: 2}))
               .pipe(rename({suffix: "-min"}))
               .pipe(sourcemaps.write())
-              .pipe(gulp.dest('build/css'))
+              .pipe(gulp.dest(path.build.style))
               .pipe(browserSync.stream());
 }
 
 function fontsCopy() {
-  return gulp.src('source/fonts/**/*.{woff,woff2}', {
+  return gulp.src(path.source.fonts, {
                 base: 'source'
               })
-              .pipe(gulp.dest('build'));
+              .pipe(gulp.dest(path.build.root));
 }
 
 function scripts() {
-  return gulp.src('source/js-source/**/*.js')
+  return gulp.src(path.source.js)
+              .pipe(rigger())
               .pipe(plumber())
-              // .pipe(babel({
-              //   presets: ['@babel/env']
-              // }))
-              .pipe(concat('script.js'))
-              // .pipe(uglify({
-              //   toplevel: true
-              // }))
-              .pipe(rename({suffix: "-min"}))
-              .pipe(gulp.dest('build/js'))
+              .pipe(sourcemaps.init())
+              .pipe(babel({
+                presets: ['@babel/env']
+              }))
+              .pipe(uglify({
+                toplevel: true
+              }))
+              .pipe(sourcemaps.write())
+              .pipe(rename('script-min.js'))
+              .pipe(sourcemaps.write())
+              .pipe(gulp.dest(path.build.js))
               .pipe(browserSync.stream());
 }
 
 function images() {
-  return gulp.src('source/img/picture/*.{jpg,png}')
+  return gulp.src(path.source.img)
               .pipe(image())
-              .pipe(gulp.dest('build/img/picture'))
+              .pipe(gulp.dest(path.build.img))
 }
 
 function webpConvert() {
-  return gulp.src('source/img/picture/*.{jpg,png}')
+  return gulp.src(path.source.img)
               .pipe(webp({
                 quality: 85,
                 method: 5
               }))
-              .pipe(gulp.dest('build/img/webp'))
+              .pipe(gulp.dest(path.build.imgWebp))
 }
 
 function svgMin() {
-  return gulp.src('source/img/icon/**/*.svg')
+  return gulp.src(path.source.svg)
         .pipe(svgmin())
-        .pipe(gulp.dest('build/img/icon'));
+        .pipe(gulp.dest(path.build.svg));
 }
 
 function createSvgSprite() {
-  return gulp.src('source/img/sprite/*.svg')
+  return gulp.src(path.source.svgSprite)
               .pipe(svgSprite({
                 mode: {
                   stack: {
@@ -101,15 +143,15 @@ function createSvgSprite() {
                   }
                 },
               }))
-              .pipe(gulp.dest('build/svg/'));
+              .pipe(gulp.dest(path.build.svgSprite));
 }
 
 function clean() {
-  return del(['build/*', '!build/img']);
+  return del([path.clean.all, path.clean.noImg]);
 }
 
 function cleanImg() {
-  return del(['build/img/*']);
+  return del([path.clean.img]);
 }
 
 function watch() {
@@ -117,10 +159,10 @@ function watch() {
     server: 'build'
   });
 
-  gulp.watch('source/less/**/*.less', styles);
-  gulp.watch('source/js-source/**/*.js', scripts);
-  gulp.watch('source/*.html', html);
-  gulp.watch('source/fonts/**/*.{woff,woff2}', fontsCopy);
+  gulp.watch(path.watch.style, styles);
+  gulp.watch(path.watch.js, scripts);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.fonts, fontsCopy);
 }
 
 gulp.task('html', gulp.series(createSvgSprite, html));
---
+++ source/js/main.js
@@ -0,0 +1,22 @@
+/*
+ * Picturefill
+ */
+//= lib/picturefill.js
+
+
+/*
+ * Навигация в шапке
+ */
+//= scripts/main-nav.js
+
+
+/*
+ * Переключатель картинки "до - после"
+ */
+//= scripts/example__mobile.js
+
+
+/*
+ * Поделючение яндекс карт
+ */
+//= scripts/jmap.js
