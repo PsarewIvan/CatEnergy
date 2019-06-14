3db355f8880443d069c7450d4ac8eac6d80a1667
Ivan Psarev <psarew.ivan@yandex.ru>
2019-06-14 23:23:45 +0300

    Доработал svg

--- source/js/scripts/main-nav.js
+++ source/js/scripts/main-nav.js
@@ -1,5 +1,6 @@
 const mainNav = document.querySelector('.js--main-nav');
 const mainNavButton = document.querySelector('.js--header__nav-btn');
+const burgerIcon = document.querySelector('js--header-burger');
 
 if (matchMedia) {
   let mqNav = window.matchMedia( '(min-width: 768px)' );
@@ -9,13 +10,8 @@ if (matchMedia) {
 
 mainNavButton.addEventListener('click', function(evt) {
   evt.preventDefault();
-  if (mainNav.classList.contains('display-none')) {
-    mainNav.classList.remove('display-none');
-    mainNavButton.classList.add('header__nav-btn--close')
-  } else {
-    mainNav.classList.add('display-none');
-    mainNavButton.classList.remove('header__nav-btn--close')
-  }
+    mainNav.classList.toggle('display-none');
+    mainNavButton.classList.toggle('header__nav-btn--close');
 });
 
 function WidthChangeNav(mq) {
