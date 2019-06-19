c70ee036a5859d77badd5d86af240a0ae947c19b
Ivan Psarev <psarew.ivan@yandex.ru>
2019-06-19 21:56:06 +0300

    Немного оптимизировал загрузку карт, добавил динамическое изменение пина при разрешении экрана

--- source/js/scripts/jmap.js
+++ source/js/scripts/jmap.js
@@ -1,36 +1,64 @@
 if (document.querySelector('#ymap')) {
-  ymaps.ready(function () {
-    let myMap = new ymaps.Map("ymap", {
-      center: [59.938631, 30.3230554],
-      zoom: 17
-    });
+  window.onload = function() {
+    ymaps.ready(function () {
+      let myMap = new ymaps.Map("ymap", {
+        center: [59.938631, 30.3230554],
+        zoom: 17
+      });
+  
+      let placemark = new ymaps.Placemark([59.938631, 30.3230554], {
+        balloonContentHeader: "Cat Energy",
+        balloonContentBody: "ул. Большая <br>Конюшенная, д. 19/8 <br>Санкт-Петербург",
+        hintContent: "Мы находимся здесь"
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/picture/map-pin.png',
+        iconImageSize: [60, 51],
+        iconImageOffset: [-30, -51]
+      });
 
-    let placemark = new ymaps.Placemark([59.938631, 30.3230554], {
-      balloonContentHeader: "Cat Energy",
-      balloonContentBody: "ул. Большая <br>Конюшенная, д. 19/8 <br>Санкт-Петербург",
-      hintContent: "Мы находимся здесь"
-    }, {
-      iconLayout: 'default#image',
-      iconImageHref: 'img/picture/map-pin.png',
-      iconImageSize: [60, 51],
-      iconImageOffset: [-30, -51]
-    });
-    
-    myMap.geoObjects.add(placemark);
-    myMap.behaviors.disable(['scrollZoom']);
+      let placemarkBig = new ymaps.Placemark([59.938631, 30.3230554], {
+        balloonContentHeader: "Cat Energy",
+        balloonContentBody: "ул. Большая <br>Конюшенная, д. 19/8 <br>Санкт-Петербург",
+        hintContent: "Мы находимся здесь"
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/picture/map-pin.png',
+        iconImageSize: [124, 106],
+        iconImageOffset: [-30, -51]
+      });
+      
+      myMap.geoObjects.add(placemark);
+      myMap.behaviors.disable(['scrollZoom']);
+  
+      if (matchMedia) {
+        let desktopWidth = window.matchMedia( '(min-width: 1300px)' );
+        let tabletWidth = window.matchMedia( '(min-width: 768px)' );
 
-    if (matchMedia) {
-      let deviceWidth = window.matchMedia( '(min-width: 1300px)' );
-      deviceWidth.addListener(setMapCenter);
-      setMapCenter(deviceWidth);
+        desktopWidth.addListener(setMapCenter);
+        tabletWidth.addListener(setBigPin);
 
-      function setMapCenter(mq) {
-        if (mq.matches) {
-          myMap.setCenter([59.938725,30.319447]);
-        } else {
-          myMap.setCenter([59.938631, 30.3230554]);
+        setMapCenter(desktopWidth);
+        setBigPin(tabletWidth);
+  
+        function setMapCenter(mq) {
+          if (mq.matches) {
+            myMap.setCenter([59.938725,30.319447]);
+          } else {
+            myMap.setCenter([59.938631, 30.3230554]);
+          }
+        }
+
+        function setBigPin(mq) {
+          if (mq.matches) {
+            myMap.geoObjects.remove(placemark);
+            myMap.geoObjects.add(placemarkBig);
+          } else {
+            myMap.geoObjects.remove(placemarkBig);
+            myMap.geoObjects.add(placemark);
+          }
         }
       }
-    }
-  });
+    });
+  };
 }
